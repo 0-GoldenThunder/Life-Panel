17596043308c15a7876a424e19001efa2a5d893a
Tsaqif <fiqats.26o4@gmail.com>
2026-06-21 21:48:09 +0800

    chore: add @astrojs/netlify dependency to package.json

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,14 +2,14 @@
 import { defineConfig } from 'astro/config';
 
 import react from '@astrojs/react';
-import node from '@astrojs/node';
+import netlify from '@astrojs/netlify';
 import tailwindcss from '@tailwindcss/vite';
 import { VitePWA } from 'vite-plugin-pwa';
 
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
-  adapter: node({ mode: 'standalone' }),
+  adapter: netlify(),
   integrations: [react()],
 
   vite: {
